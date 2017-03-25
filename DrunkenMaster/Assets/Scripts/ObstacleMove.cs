6eeedfba9afec97266808f50e7184439635e42aa
zwu002 <zwu002@gold.ac.uk>
2017-03-25 13:13:27 +0000

    Added movement to obstacles

---
+++ DrunkenMaster/Assets/Scripts/ObstacleMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleMove : MonoBehaviour {
+
+    public float speed;
+
+    Vector3 position;
+
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        position = gameObject.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        position.z -= 0.02f;
+
+        transform.position = position;
+    }
+}
