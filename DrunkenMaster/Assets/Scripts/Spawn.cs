84e8362d3044e87512ea398bd728501ab37df1f1
zwu002 <zwu002@gold.ac.uk>
2017-03-25 18:17:07 +0000

    Adjusted spawning (3 columns)

--- DrunkenMaster/Assets/Scripts/Spawn.cs
+++ DrunkenMaster/Assets/Scripts/Spawn.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class Spawn : MonoBehaviour {
 
-    public GameObject[] obstacle;
+    public GameObject[] spawnObstacle;
 
-    public float randomX;
-    public float randomZ;
+    public int randomPos;
+    public float pos;
+    public float minPosZ;
+    public float maxPosZ;
+
+    float timer;
+    int obstacleIndex;
 
 	void Start () {
-	  
+        timer = 2f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            obstacleIndex = Random.Range(0, 3);
+            randomPos = Random.Range(0, 3);
+
+            if (randomPos == 0)
+            {
+                pos = -0.5f;
+            }
+            else if (randomPos == 1)
+            {
+                pos = 0;
+            }
+            else
+            {
+                pos = 0.5f;
+            }
+
+            Instantiate(spawnObstacle[obstacleIndex], new Vector3(pos, 0.15f, Random.Range(minPosZ, maxPosZ)), Quaternion.identity);
+ 
+            timer = Random.Range(0.2f, 2f);
+        }
 	}
 }
