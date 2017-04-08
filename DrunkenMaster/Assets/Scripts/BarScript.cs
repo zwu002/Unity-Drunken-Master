3c34ef566686bcf6f4335ca558b8d4579088a9f4
zwu002 <zwu002@gold.ac.uk>
2017-04-08 13:05:47 +0100

    Added drunkness bar

---
+++ DrunkenMaster/Assets/Scripts/BarScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    [SerializeField]
+    float fillAmount;
+
+    [SerializeField]
+    Image mask;
+
+    GameObject uiManager;
+
+	// Use this for initialization
+	void Start () {
+        uiManager = GameObject.Find("UIManager");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        fillAmount = uiManager.GetComponent<UIManager>().drunkness / 100;
+        HandleBar();
+	}
+
+    void HandleBar()
+    {
+        mask.fillAmount = fillAmount;
+    }
+}
