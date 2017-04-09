ab405202cbbc5fd861dc45246c16b1b2606ee533
zwu002 <zwu002@gold.ac.uk>
2017-04-09 13:47:49 +0100

    Imported textures for characters. Kill celebrating sprites after 1.5s

---
+++ DrunkenMaster/Assets/Scripts/SpriteDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteDestroy : MonoBehaviour {
+
+    float timer;
+    float destroyTime;
+
+	// Use this for initialization
+	void Start () {
+        timer = Time.time;
+        destroyTime = 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (Time.time - timer > destroyTime)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
